5fb3b1c175b85d4bcf190e63a85e98ef7e3cb3fa
Adriano Almeida <adriano@taskka.com.br>
2018-09-16 21:59:27 -0300

    categorias seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
-         $this->call(StateTableSeeder::class);
-         $this->call(CitiesTableSeeder::class);
+//         $this->call(UsersTableSeeder::class);
+//         $this->call(StateTableSeeder::class);
+//         $this->call(CitiesTableSeeder::class);
          $this->call(CategoriesTableSeeder::class);
     }
 }
