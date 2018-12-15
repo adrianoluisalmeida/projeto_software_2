a2606a0ae45b98d57eaaea55f37f1069d90c7cb8
Roger Eduardo de Couto <rogerecouto@gmail.com>
2018-12-15 13:54:25 -0200

    Melhorado layout do filtro de reports

--- resources/views/admin/reports/index.blade.php
+++ resources/views/admin/reports/index.blade.php
@@ -13,25 +13,41 @@
     </li>
 @endsection
 
+@push('scripts')
+    <script type="text/javascript">
+        function filter() {
+            var status = document.getElementById('select_status').value;
+            var category = document.getElementById('select_category').value;
+            document.getElementById('hidden_status').value = status;
+            document.getElementById('hidden_category').value = category;
+            document.getElementById('form_filtro').submit();
+        }
+    </script>
+@endpush
+
 @section('content')
-    <div class="form-group" style="background-color: #ececec; padding: 10px;">
-        <form method="GET" action="{{route('reports.index')}}">
-            <label>Situação</label>
-            <select name="status" class="form-control" onchange="this.form.submit()">
-                <option value="0" @if(!isset($status)||$status==0) selected @endif>Todas</option>
-                <option value="1" @if(isset($status)&&$status==1) selected @endif>Abertas</option>
-                <option value="2" @if(isset($status)&&$status==2) selected @endif>Em andamento</option>
-                <option value="3" @if(isset($status)&&$status==3) selected @endif>Concluídas</option>
-            </select>
-            <label>Categoria</label>
-            <select name="category" class="form-control" onchange="this.form.submit()">
-                <option value="0" @if(!isset($category)||$category==0) selected @endif>Todas</option>
-                @foreach($categories as $categ)
-                    <option value="{{$categ->id}}" @if(isset($category)&&$category==$categ->id) selected @endif>{{$categ->name}}</option>
-                @endforeach
-            </select>
-        </form>
+    <form id="form_filtro" method="GET" action="{{route('reports.index')}}">
+        <input id="hidden_status" type="hidden" name="status" />
+        <input id="hidden_category" type="hidden" name="category" />
+    </form>
+    <div class="input-group col-md-6" style="margin-left: 15px;padding: 5px;background-color: #e4e4e4;">
+
+        <span class="input-group-addon" title="Situação" id="situacaoLabel"> <i class="fa fa-filter"></i>&nbsp;&nbsp;&nbsp;Situação:&nbsp;</span>
+        <select id="select_status" name="status" class="form-control" onchange="filter()">
+            <option value="0" @if(!isset($status)||$status==0) selected @endif>Todas</option>
+            <option value="1" @if(isset($status)&&$status==1) selected @endif>Abertas</option>
+            <option value="2" @if(isset($status)&&$status==2) selected @endif>Em andamento</option>
+            <option value="3" @if(isset($status)&&$status==3) selected @endif>Concluídas</option>
+        </select>
+        <span class="input-group-addon" title="Situação" id="situacaoLabel">Categoria: </span>
+        <select id="select_category" name="category" class="form-control" onchange="filter()">
+            <option value="0" @if(!isset($category)||$category==0) selected @endif>Todas</option>
+            @foreach($categories as $categ)
+                <option value="{{$categ->id}}" @if(isset($category)&&$category==$categ->id) selected @endif>{{$categ->name}}</option>
+            @endforeach
+        </select>
     </div>
+
     @foreach($entities as $entity)
     <div class="col-md-12">
         <h2><small>Entidade </small>{{ $entity->name }}</h2>
@@ -47,6 +63,7 @@
                     <td style="width: 12%;">Situação</td>
                     <td style="width: 5%;"><i class="fa fa-thumbs-up"></i></td>
                     <td style="width: 5%;"><i class="fa fa-thumbs-down"></i></td>
+                    <td></td>
                 </tr>
                 </thead>
                 <tbody>
